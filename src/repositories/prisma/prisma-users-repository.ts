413a7f97036b51bdb598f14386f01932c4636516
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-01-22 10:36:29 -0300

    feat: create prisma repositories

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,19 @@
 import { prisma } from "@/lib/prisma"
-import { Prisma, User } from "@prisma/client"
+import { Prisma } from "@prisma/client"
 
 import { UsersRepository } from "../users-repository"
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(id: string): Promise<User | null> {
-    throw new Error("Method not implemented.")
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -16,6 +23,7 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user
   }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
