4c72194f25ef4b9719863e974f38eead3b5649b7
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-01-27 11:54:28 -0300

    feat: refresh token & RBAC

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,26 @@ import fastifyJwt from "@fastify/jwt"
 import { usersRoutes } from "./http/controllers/users/routes"
 import { gymsRoutes } from "./http/controllers/gyms/routes"
 import { checkInsRoutes } from "./http/controllers/check-ins/routes"
+import fastifyCookie from "@fastify/cookie"
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
-
-app.register(fastifyJwt, { secret: env.JWT_SECRET })
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
